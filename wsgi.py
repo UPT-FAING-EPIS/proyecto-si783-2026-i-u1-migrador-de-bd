72d2cbc6926650b24bb9a14eba4441df6eb49931
Usher Halanocca Rojas <uh2023076795@virtual.upt.pe>
2026-05-08 16:56:05 -0500

    Update wsgi.py for SocketIO and Procfile to use wsgi:app

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from app import crear_app
+from app import crear_app, socketio
+import os
 
 app = crear_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, use_reloader=False)
